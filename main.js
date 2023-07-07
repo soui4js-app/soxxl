4c06fe01d922d600c897d624f49f56b69c6053c1
huangjianxiong1 <huangjianxiong1@yy.com>
2023-07-07 18:25:57 +0800

    init

---
+++ main.js
@@ -0,0 +1,87 @@
+﻿import * as soui4 from "soui4";
+import {R} from "uires/R.js";
+
+var g_workDir="";
+
+const base_id = 1000;
+class MainDialog extends soui4.JsHostWnd{
+	constructor(){
+		super("layout:dlg_main");
+		this.onEvt = this.onEvent;
+	}
+
+	onEvent(e){
+		if(e.GetID()==soui4.EVT_INIT){//event_init
+			this.init();
+		}else if(e.GetID()==soui4.EVT_EXIT){
+			this.uninit();
+		}
+		return false;
+	}
+	
+	initBoard(){
+		let wndBoard = this.FindIChildByID(R.id.wnd_board);
+		const kBoardSize={row:7,col:7};
+		wndBoard.DestroyAllChildren();
+		wndBoard.SetAttribute("columnCount",""+kBoardSize.col,false);
+	
+		let head="<t:g.ele>";
+		let tail="</t:g.ele>";
+		let xml="";
+		let eles = kBoardSize.row * kBoardSize.col;
+		for(let i=0;i<eles;i++){
+			let ele = "<data id=\""+ (base_id+i) +"\"/>";
+			xml += head+ele+tail;
+		}
+
+		wndBoard.CreateChildrenFromXml(xml);
+		wndBoard.RequestRelayout();
+	}
+
+	init(){
+		console.log("init");
+		this.initBoard();
+	}
+	uninit(){
+		//do uninit.
+		console.log("uninit");
+	}
+};
+
+
+function main(inst,workDir,args)
+{
+	soui4.log(workDir);
+	g_workDir = workDir;
+	let theApp = soui4.GetApp();
+	let res = theApp.InitXmlNamedID(R.name_arr,R.id_arr);
+	console.log("InitXmlNamedID ret:",res);
+	let souiFac = soui4.CreateSouiFactory();
+	//*
+	let resProvider = souiFac.CreateResProvider(1);
+	soui4.InitFileResProvider(resProvider,workDir + "\\uires");
+	//*/
+	/*
+	// show how to load resource from a zip file
+	let resProvider = soui4.CreateZipResProvider(theApp,workDir +"\\uires.zip","souizip");
+	if(resProvider === 0){
+		soui4.log("load res from uires.zip failed");
+		return -1;
+	}
+	//*/
+	let resMgr = theApp.GetResProviderMgr();
+	resMgr.AddResProvider(resProvider,"uidef:xml_init");
+	resProvider.Release();
+	let hwnd = soui4.GetActiveWindow();
+	let hostWnd = new MainDialog();
+	hostWnd.Create(hwnd,0,0,0,0);
+	hostWnd.SendMessage(0x110,0,0);//send init dialog message.
+	hostWnd.ShowWindow(soui4.SW_SHOWNORMAL); 
+	souiFac.Release();
+	let ret= theApp.Run(hostWnd.GetHwnd());
+	hostWnd=null;
+	soui4.log("js quit");
+	return ret;
+}
+
+globalThis.main=main;
